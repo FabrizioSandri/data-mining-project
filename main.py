b27f28dbd6c57f41224f4159102f148efe8e4f86
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-12-10 22:17:37 +0100

    recommendation system performance

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ import time
 import matplotlib.pyplot as plt
 
 #### VARIABLES
-utility_df = pd.read_csv("tests/Dataset/dataFolder/utilityMatrixDataset.csv")
-# utility_df = pd.read_csv("utility.csv", header=None, index_col=False)
-utility = utility_df.to_numpy()
+# utility_df = pd.read_csv("tests/Dataset/dataFolder/utilityMatrixDataset.csv")
+utility_df = pd.read_csv("utility.csv", header=None, index_col=False)
+original_utility = utility_df.to_numpy()
 
 # fill some random values of the utility with 0. based on the sparsity amount
 utility_sparsity = 0.3
-mask = np.random.choice([True, False], utility.shape , p=[utility_sparsity,1-utility_sparsity])
+mask = np.random.choice([True, False], original_utility.shape , p=[utility_sparsity,1-utility_sparsity])
+utility = original_utility.copy()
 utility[mask] = 0
 
 
@@ -23,8 +24,8 @@ utility[mask] = 0
 # They are there just as a reference to be used for testing.
 
 ### LSH with minHash
-# signature_matrix = lsh.minHash(utility_matrix=utility, k=200, T=50)
-# similar_items = lsh.lsh(signature_matrix, bands=50)
+signature_matrix = lsh.minHash(utility_matrix=utility, k=400, T=50)
+similar_items = lsh.lsh(signature_matrix, rows_per_band=10)
 
 
 ### LSH with simHash
@@ -61,49 +62,44 @@ utility[mask] = 0
 # This part plots the time complexity of the four algorithms(jaccard similarity
 # without LSH, cosine similarity without LSH, LSH with minHash and LSH with 
 # simHash). 
-num_queries_j = np.arange(start=100, stop=1100, step=100)
-num_users = np.arange(start=100, stop=600, step=100)
 
-time_jaccard_q, _, time_minhash_q, _ = ev.measure_time_increase(utility, num_queries=num_queries_j, algorithms="jaccard")
-time_jaccard_u, _, time_minhash_u, _ = ev.measure_time_increase(utility, num_users=num_users, num_queries=[1000], algorithms="jaccard")
+# num_queries_j = np.arange(start=100, stop=1100, step=100)
+# num_queries_c = np.arange(start=300, stop=3000, step=200) # test cosine similarity and simhash with more queries
+# num_users = np.arange(start=100, stop=600, step=100)
 
-num_queries_c = np.arange(start=300, stop=3000, step=200) # test cosine similarity and simhash with more queries
-_, time_cosine_q, _, time_simhash_q = ev.measure_time_increase(utility, num_queries=num_queries_c, algorithms="cosine")
-_, time_cosine_u, _, time_simhash_u = ev.measure_time_increase(utility, num_users=num_users, algorithms="cosine")
+# ev.plot_time_increase(utility, num_users, num_users, num_queries_j, num_queries_c)
 
-fig,ax = plt.subplots(2,2)
-ax = ax.flatten()
 
-ax[0].plot(num_queries_j, time_jaccard_q, label="Baseline solution Jaccard")
-ax[0].plot(num_queries_j, time_minhash_q, label="LSH solution Minhash")
-ax[0].set_xlabel("Number of queries (utility matrix columns)")
-ax[0].set_ylabel("Time (seconds)")
-ax[0].legend()
-ax[0].grid()
-ax[0].title.set_text("LSH for Jaccard - increasing the number of queries")
+################################################################################
+# In this part the recommendation system actually operates and the utility 
+# matrix is filled with the missing values, taking the value from the most 
+# similar query for a user 
+similarity = "cosine"
 
-ax[2].plot(num_queries_c, time_cosine_q, label="Baseline solution Cosine")
-ax[2].plot(num_queries_c, time_simhash_q, label="LSH solution Simhash")
-ax[2].set_xlabel("Number of queries (utility matrix columns)")
-ax[2].set_ylabel("Time (seconds)")
-ax[2].legend()
-ax[2].grid()
-ax[2].title.set_text("LSH for Cosine - increasing the number of queries")
+for query in range(utility.shape[1]):
+  print("Predicting missing ratings for query %d" % query)
+  for user in range(utility.shape[0]):
+    if utility[user, query] == 0:
 
-ax[1].plot(num_users, time_jaccard_u, label="Baseline solution Jaccard")
-ax[1].plot(num_users, time_minhash_u, label="LSH solution Minhash")
-ax[1].set_xlabel("Number of users (utility matrix rows)")
-ax[1].set_ylabel("Time (seconds)")
-ax[1].legend()
-ax[1].grid()
-ax[1].title.set_text("LSH for Jaccard - increasing the number of users")
+      prefs = []
+      for i in similar_items[query]:
+        if similarity == "jaccard":
+          prefs.append(sim.jaccard_threshold(utility[:,query], utility[:,i], T=50))
+        else:
+          prefs.append(sim.cosine_similarity(utility[:,query], utility[:,i]))
+      
+      most_similar_query = None
+      if len(similar_items[query]) > 0: # at least one similar query found
+        most_similar_query_i = np.argmax(prefs)
+        most_similar_query = list(similar_items[query])[most_similar_query_i]
 
-ax[3].plot(num_users, time_cosine_u, label="Baseline solution Cosine")
-ax[3].plot(num_users, time_simhash_u, label="LSH solution Simhash")
-ax[3].set_xlabel("Number of users (utility matrix rows)")
-ax[3].set_ylabel("Time (seconds)")
-ax[3].legend()
-ax[3].grid()
-ax[3].title.set_text("LSH for Cosine - increasing the number of users")
+      if most_similar_query:
+        utility[user, query] = utility[user, most_similar_query]
 
-plt.show()
+
+# dump the predicted utility matrix to file
+pd.DataFrame(utility).to_csv("predicted.csv", header=False, index=False)
+
+# measure the average difference between the predicted ratings and the originals
+avg = ev.evaluate_prediction(original_utility, utility, mask)
+print("The predicted ratings differ from the real ones by an average of %f" % np.mean(avg))

a9d26f8bc7ee641e330605a50260c16fe6def45a
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-12-22 20:54:02 +0100

    main fill_na and fixed slow similarity search loop

--- main.py
+++ main.py
@@ -8,16 +8,11 @@ import time
 import matplotlib.pyplot as plt
 
 #### VARIABLES
-utility_df = pd.read_csv("tests/Dataset/dataFolder/UtilityDataset.csv", index_col=0)
-original_utility = utility_df.to_numpy()
+utility_df = pd.read_csv("tests/Dataset/dataFolder/UtilityDataset_Synthetic.csv", index_col=0)
+utility_df.fillna(0, inplace=True)
+utility = utility_df.to_numpy()
 
-K = 5 # predict the rating with the rating of the K most similar queries
-
-# fill some random values of the utility with 0. based on the sparsity amount
-utility_sparsity = 0.3
-mask = np.random.choice([True, False], original_utility.shape , p=[utility_sparsity,1-utility_sparsity])
-utility = original_utility.copy()
-utility[mask] = 0
+K = 3 # predict the rating with the rating of the K most similar queries
 
 
 ################################################################################
@@ -25,13 +20,13 @@ utility[mask] = 0
 # They are there just as a reference to be used for testing.
 
 ### LSH with minHash
-signature_matrix = lsh.minHash(utility_matrix=utility, k=400, T=50)
-similar_items = lsh.lsh(signature_matrix, rows_per_band=7)
+# signature_matrix = lsh.minHash(utility_matrix=utility, k=400, T=50)
+# similar_items = lsh.lsh(signature_matrix, rows_per_band=7)
 
 
 ### LSH with simHash
-# signature_matrix = lsh.simHash(utility_matrix=utility, hyperplanes=400)
-# similar_items = lsh.lsh(signature_matrix, rows_per_band=15)
+signature_matrix = lsh.simHash(utility_matrix=utility, hyperplanes=400)
+similar_items = lsh.lsh(signature_matrix, rows_per_band=15)
 
 ################################################################################
 # In this part we plot the time to run LSH as well as the number of correctly 
@@ -73,30 +68,37 @@ similar_items = lsh.lsh(signature_matrix, rows_per_band=7)
 
 ################################################################################
 # In this part the recommendation system actually operates and the utility 
-# matrix is filled with the missing values, taking the value from the most 
+# matrix is filled with the missing values, taking the value from the K most 
 # similar query for a user 
 similarity = "cosine"
 
+
+# this part finds the K-most similar queries for each query
+k_most_similar = {}
+for query in range(utility.shape[1]):
+  prefs = []
+  for i in similar_items[query]:
+    if similarity == "jaccard":
+      prefs.append(sim.jaccard_threshold(utility[:,query], utility[:,i], T=50))
+    else:
+      prefs.append(sim.cosine_similarity(utility[:,query], utility[:,i]))
+  
+  k_most_similar[query] = []
+  if len(similar_items[query]) > 0: # at least one similar query found
+    most_similar_query_i = np.argsort(prefs)[min(-K,len(prefs)):]
+    k_most_similar[query] =  [list(similar_items[query])[i] for i in most_similar_query_i]
+
+
+
+# now the recommendation system computes the missing values as the average of 
+# the K most similar queries 
 for query in range(utility.shape[1]):
   print("Predicting missing ratings for query %d" % query)
   for user in range(utility.shape[0]):
-    if utility[user, query] == 0:
+    if utility[user, query] == 0 and len(k_most_similar[query]) > 0:
+      similar_ratings = [utility[user,j] for j in k_most_similar[query]]
+      utility[user, query] = round(np.mean(similar_ratings))
 
-      prefs = []
-      for i in similar_items[query]:
-        if similarity == "jaccard":
-          prefs.append(sim.jaccard_threshold(utility[:,query], utility[:,i], T=50))
-        else:
-          prefs.append(sim.cosine_similarity(utility[:,query], utility[:,i]))
-      
-      most_similar_queries = None
-      if len(similar_items[query]) > 0: # at least one similar query found
-        most_similar_query_i = np.argsort(prefs)[min(-K,len(prefs)):]
-        most_similar_queries =  [list(similar_items[query])[i] for i in most_similar_query_i]
-
-      if most_similar_queries:
-        similar_ratings = [utility[user,j] for j in most_similar_queries]
-        utility[user, query] = np.mean(similar_ratings)
 
 
 # dump the predicted utility matrix to file
@@ -105,12 +107,12 @@ predicted_utility_df.columns = utility_df.keys()
 predicted_utility_df.index = utility_df.index
 predicted_utility_df.to_csv("predicted.csv")
 
-# measure the average difference between the predicted ratings and the originals
-avg = ev.evaluate_prediction(original_utility, utility, mask)
-print("The predicted ratings differ from the real ones by an average of %f" % np.mean(avg))
+# # measure the average difference between the predicted ratings and the originals
+# avg = ev.evaluate_prediction(original_utility, utility, mask)
+# print("The predicted ratings differ from the real ones by an average of %f" % np.mean(avg))
 
 
-# compare the results with random assignment of the ratings
-fake_utility = np.random.randint(1,101, utility.shape) # utility with random values
-avg_fake = ev.evaluate_prediction(fake_utility, utility, mask)
-print("The predicted ratings differ from the randomly generated predictions by an average of %f" % np.mean(avg_fake))
+# # compare the results with random assignment of the ratings
+# fake_utility = np.random.randint(1,101, utility.shape) # utility with random values
+# avg_fake = ev.evaluate_prediction(fake_utility, utility, mask)
+# print("The predicted ratings differ from the randomly generated predictions by an average of %f" % np.mean(avg_fake))
